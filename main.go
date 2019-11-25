1c7bc1a61f7c47f61af43c3964e3a5599ef8873e
DrewFrost <drew.vfrost@gmail.com>
2019-11-25 19:15:10 +0200

    quiz with timer

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 type problem struct {
@@ -25,7 +26,8 @@ func parseLines(lines [][]string) []problem {
 }
 
 func main() {
-	csvFilename := flag.String("csv", "problems.csv", "a csv file of problems 'question, answer' ")
+	csvFilename := flag.String("csv", "problems.csv", "a csv file of problems in format:'question, answer' ")
+	timeLimit := flag.Int("limit", 30, "the time limit for the quiz in seconds")
 	flag.Parse()
 	file, err := os.Open(*csvFilename)
 	if err != nil {
@@ -37,13 +39,26 @@ func main() {
 		error(fmt.Sprintf("Failed to parse csv file: %s \n", *csvFilename))
 	}
 	problems := parseLines(lines)
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
+
 	correct := 0
+problemLoop:
 	for i, p := range problems {
 		fmt.Printf("Problem #%d: %s = ", i+1, p.q)
-		var answer string
-		fmt.Scanf("%s\n", &answer)
-		if answer == p.a {
-			correct++
+		answerCh := make(chan string)
+		go func() {
+			var answer string
+			fmt.Scanf("%s\n", &answer)
+			answerCh <- answer
+		}()
+		select {
+		case <-timer.C:
+			fmt.Println()
+			break problemLoop
+		case answer := <-answerCh:
+			if answer == p.a {
+				correct++
+			}
 		}
 	}
 	fmt.Printf("You have %d answers correct out of %d.\n", correct, len(problems))
